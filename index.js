c4e523e93a482401a75f854b2d88679d99ae2a1f
Yuma Iwami <cjuza1031@gmail.com>
2020-03-16 11:50:44 +0900

    minor change
    minor change

--- index.js
+++ index.js
@@ -5,5 +5,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { NavBar } from "./navBar";
+import { MouseTracker } from "./mouseTracker";
+import { ShowGraph } from "./showGraph";
 
-ReactDOM.render(<NavBar />, document.getElementById("root"));
+class MainController extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodePosition: []
+    };
+    this.makeNode = this.makeNode.bind(this);
+    this.clearNodes = this.clearNodes.bind(this);
+  }
+
+  makeNode(x, y) {
+    let nodePosition = this.state.nodePosition;
+    this.setState({
+      nodePosition: nodePosition.concat({
+        x: x,
+        y: y
+      })
+    });
+    console.log(this.state.nodePosition);
+  }
+
+  clearNodes() {
+    this.setState({
+      nodePosition: []
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar clear={this.clearNodes} />
+        <MouseTracker makeNode={this.makeNode} />
+        <ShowGraph node={this.state.nodePosition} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<MainController />, document.getElementById("root"));
--- navBar.js
+++ navBar.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
 import LOGO from "./logo.svg";
 
 export class NavBar extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClear = this.handleClear.bind(this);
   }
+
+  handleClear(e) {
+    this.props.clear();
+  }
+
   render() {
     return (
       <div>
@@ -14,6 +20,16 @@ export class NavBar extends React.Component {
             <img src={LOGO} width="30" height="30" alt="React Bootstrap logo" />
             Shortest Path Algorithms
           </Navbar.Brand>
+          <Button variant="outline-success" className="nav-button">
+            Go!
+          </Button>
+          <Button
+            variant="outline-danger"
+            className="nav-button"
+            onClick={this.handleClear}
+          >
+            Clear
+          </Button>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
